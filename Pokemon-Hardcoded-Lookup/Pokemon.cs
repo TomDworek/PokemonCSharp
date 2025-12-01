209705a0890f658e13491ad6d1e17c1256bec84b
Tom <tomgaming@hotmail.no>
2025-12-01 20:46:56 +0100

    Add project files.

---
+++ Pokemon-Hardcoded-Lookup/Pokemon.cs
@@ -0,0 +1,12 @@
+namespace Pokemon;
+
+public class Pokemon
+{
+    // du vet hvordan dette fungerer allerede hahah
+    public string idPokedex { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Level { get; set; }
+    public int Health { get; set; }
+    
+}
